77916e40d6208b60a9c71d6116ac40b750f32444
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-02-20 14:46:01 +0100

    Nye opdateringer

---
+++ main.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 225) {
+        $(".scrolllogo").addClass("showscroll");
+        $(".project-nav").addClass("showscroll");
+        $(".justify").addClass("nav-scroll");
+    } else {
+        $(".scrolllogo").removeClass("showscroll");
+        $(".project-nav").removeClass("showscroll");
+        $(".justify").removeClass("nav-scroll");
+    }
+});
