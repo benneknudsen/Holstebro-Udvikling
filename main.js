0c87719f8920066e6dd215ed7c86b5e9d69dd1b2
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-02-25 13:58:05 +0100

    Responsive - Mobil fix - navbar

--- main.js
+++ main.js
@@ -12,3 +12,93 @@ $(window).scroll(function() {
     }
 });
 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+
+    }
+  });
+}
+
+
+
+
+
+$(document).ready(function () {
+  $('#drawer-toggle').click(function () {
+    $('#nav-icon1').toggleClass('open');
+  });
+});
+
+
+$(document).ready(function () {
+  $('#drawer-togglem').click(function () {
+    $('#nav-icon1m').toggleClass('open');
+  });
+});
+
+
+var dropdown1 = document.getElementsByClassName("dropdown-btn-m");
+var i;
+
+for (i = 0; i < dropdown1.length; i++) {
+  dropdown1[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+$(".rotate").click(function () {
+  $(this).toggleClass("down");
+})
+
+$(document).ready(function () {
+  $('.dropdown-btn').click(function () {
+    $('.dropdown-container').toggleClass('up');
+  });
+});
+
+$(document).ready(function () {
+  $('#drawer-toggle').click(function () {
+    $('.blur').toggleClass('blurry');
+  });
+});
+
+$(document).ready(function () {
+  $('#drawer-togglem').click(function () {
+    $('.blur').toggleClass('blurry');
+  });
+});
+
+
+$(document).ready(function () {
+  $('.dropdown-btn-m').click(function () {
+    $('.dropdown-container').toggleClass('up');
+  });
+});
+
+$(document).ready(function () {
+  $('.dropdown-btn').click(function () {
+    $('.roter').toggleClass('ned');
+  });
+});
+
+$(document).ready(function () {
+  $('.dropdown-btn-m').click(function () {
+    $('.roter').toggleClass('ned');
+  });
+});
